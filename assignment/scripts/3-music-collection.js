c0f6feee2665970e1b68a3ca4bc7a70370df4caa
Lee Baesler <lee.baesler@gmail.com>
2022-08-20 21:58:09 -0500

    first-commit

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
-console.log('***** Music Collection *****')
+
+const collection=[];
+
+function addToCollection (title, artist, yearPublished){
+
+let records = (addToCollection);
+
+collection.push(records);
+}
+console.log('***** Music Collection *****');
