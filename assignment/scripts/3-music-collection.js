08a551845604ffadea1cb41a459b91a03975cfbb
Lee Baesler <lee.baesler@gmail.com>
2022-08-21 01:25:29 -0500

    third-commit

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,9 +1,52 @@
 
 const collection=[];
-
-function addToCollection (title, artist, yearPublished) {
-    const obj = {addToCollection};
-    obj.push;
+function addToCollection (title, artist, yearPublished){
+    let obj = (title, artist, yearPubished)
+    obj.push(title, artist, yearPublished);
     return obj;
 }
-console.log('ok')
+console.log ('ok');
+
+let artistOne = {
+    title: 'Mariah Carey',
+    artist: 'Mariah Carey',
+    yearPublished: 1991
+
+}
+console.log (artistOne);
+
+let artistTwo = {
+    title: 'The Bodyguard',
+    artist: 'Whitney Houston',
+    yearPublished: 1992
+}
+console.log(artistTwo);
+
+let artistThree = {
+    title: 'The Lion King',
+    artist: 'Elton John',
+    yearPubsished: 1993
+}
+console.log(artistThree);
+
+let artistFour = {
+    title: 'Some Gave All',
+    artist: 'Billy Ray Cyrus',
+    yearPublished: 1994
+}
+console.log(artistFour);
+
+let artistFive = {
+    title: 'Cracked Rearview',
+    artist: 'Hootie & the Blowfish',
+    yearPublished: 1995
+}
+console.log(artistFive);
+
+let artistSix = {
+    title: 'Jagged Little Pill',
+    artist: 'Alanis Morissette',
+    yearPublished: 1996
+}
+console.log(artistSix);
+
